0bf14355e9904a1344b07803ca37f535e7594bfd
moshiurshimul <hello@moshiurshimul.com>
2020-09-02 04:19:13 +0600

    nav improve

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.css';
 import Header from './components/Header/Header';
 import Shop from './components/Shop/Shop';
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import OrderReview from './components/OrderReview/OrderReview'
 import Invantory from './components/Inventory/Invantory';
 import Notfound from './components/Notfound/Notfound';
@@ -13,6 +13,12 @@ function App() {
     <div className='App'>
      <Header></Header>
      <Router>
+         <nav className="main-nav">
+            <Link to="/shop">Shop</Link>
+            <Link to="/review">Order Review</Link>
+            <Link to="/manage">Manage Inventory</Link>
+         </nav>
+
        <Switch>
          <Route path="/shop">
           <Shop></Shop>
